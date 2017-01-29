bb056f29915f8a24c15fabd0da5148fecb3db51c
lromeo <lromeo@gmail.com>
2017-01-29 01:18:23 -0500

    add standings

---
+++ R/standings.R
@@ -0,0 +1,97 @@
+
+standings <- R6::R6Class(
+    "standings",
+    public = list(
+
+        data = NA,
+        season = NA,
+
+        initialize = function() {
+            self$data <- serie_a
+        },
+
+        initialize_standings = function() {
+            by_match_day = function(match_day, data) {
+                 data <- data %>%
+                     select(p_team) %>%
+                     mutate(position = NA, points = NA, matches_played = NA)
+                 if(match_day == 0) {
+                     data <- data  %>%
+                         mutate(points = 0, matches_played = 0)
+                 }
+                 return(data)
+            }
+            by_season <- function(ratings) {
+                ratings %>%
+                    mutate(
+                       data = purrr::map2(match_day, data, .f = by_match_day))
+            }
+
+            self$data <- self$data %>%
+                mutate(standings = purrr::map(ratings, by_season))
+        },
+
+        get_match_day_results = function(season, match_day) {
+            self$data %>%
+                filter(season == get("season")) %>%
+                tidyr::unnest(results) %>%
+                filter(match_day == get("match_day")) %>%
+                tidyr::unnest(data)
+        },
+
+        get_match_day_standings = function(season, match_day) {
+            self$data %>%
+                filter(season == get("season")) %>%
+                select(standings) %>%
+                tidyr::unnest() %>%
+                filter(match_day == get("match_day")) %>%
+                select(data) %>%
+                tidyr::unnest()
+        },
+
+        update_match_day = function(match_day) {
+
+
+            match_day_results <- self$get_match_day_results(self$season, match_day)
+            previous_standings <- self$get_match_day_standings(self$season,
+                                                        match_day - 1)
+
+            match_day_results <- match_day_results %>%
+                mutate(
+                    match_played = !is.na(points),
+                    points = ifelse(!match_played, 0, points))
+
+
+            updated_standings <- previous_standings %>%
+                left_join(match_day_results %>%
+                              select(p_team, result_points = points, match_played),
+                          by = "p_team") %>%
+                mutate(
+                    points = points + result_points,
+                    matches_played = matches_played + match_played) %>%
+                arrange(desc(points)) %>%
+                mutate(position = seq(nrow(.))) %>%
+                select(-result_points, -match_played)
+
+            self$data$standings[[self$season]] <- self$data$standings[[self$season]] %>%
+                mutate(data = purrr::map2(
+                    match_day, data,
+                    ~ if(.x == match_day) updated_standings else .y))
+        },
+
+        update_season = function(season) {
+            self$season <- season
+            matches <- seq(self$data$match_days_complete[self$season])
+            purrr::walk(matches, self$update_match_day)
+        },
+
+        update_all_seasons = function() {
+            self$initialize_standings()
+            purrr::walk(seq(nrow(self$data)), self$update_season)
+            serie_a <- self$data
+            save(serie_a, file = "data/serie_a.rData")
+        }
+
+    )
+)
+
